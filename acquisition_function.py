c71a9d37cb973e0a75e6f893333b014e04732ca1
scakolli <134518915+scakolli@users.noreply.github.com>
2023-07-05 15:59:49 +0200

    m

--- acquisition_function.py
+++ acquisition_function.py
@@ -25,33 +25,3 @@ def F_acqusition(y, std, kappa, return_array=False):
     else:
         # I think we should return [utility, std_utility]?
         return np.array([Facq, std_utility]).T
-
-    
-
-# def get_F(y, ideal_points=[0.0,-5.1], weights=[1.0,0.7],
-#                            std=[], kappa=1., return_array=False):
-    
-#     ''' Compute utility function F and acquisition function Facq = F + k*\sigma '''
-
-#     y[0]/=1000. # lambda to eV
-    
-#     utility_hole = -np.sqrt(( (y[0]-ideal_points[0]) * 1.0)**2 + ( (y[1]-ideal_points[1]) * 0.7)**2) 
-    
-#     if len(std)>0:
-        
-#         # for compatiblity with order in AL 
-#         std[0]/=1000.
-#         var_hole = np.sum( utility_hole**(-2) * np.array([weights[0], weights[1]])**4 * \
-#                           (np.array([ y[0], y[1] ]) - np.array( [ideal_points[0],ideal_points[1]] ))**2 * \
-#                            np.array( [std[0],std[1]] )**2 )
-        
-#         std_hole = np.sqrt(var_hole)
-#         utility_hole += kappa*std_hole
-
-#         if return_array:
-#             return [ utility_hole, std_hole ]
-    
-#     if return_array:
-#         return [ utility_hole, 0 ]
-    
-#     return utility_hole
